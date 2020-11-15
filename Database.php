a19844902d4665f1432c53e259927cef8753bcb5
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-11-15 03:33:05 +0300

    Creating a function FileCreation

--- Database.php
+++ Database.php
@@ -4,6 +4,8 @@
         private $fileName = "users.xml";
         private $isFileExists;
         
+        private $dom;
+        
         public function __construct(){
             $this->isFileExists = self::IsFileExists();
         }
@@ -11,6 +13,18 @@
         //Проверка существования XML файла
         private function IsFileExists(){
             return file_exists($this->fileName);
+        }
+        
+        //Создание XML файла
+        private function FileCreation(){
+            $this->dom = new domDocument("1.0", "utf-8");//Создаём XML-документ версии 1.0 с кодировкой utf-8
+            
+            $root = $this->dom->createElement("users");//Создаём корневой элемент
+            $root->setAttribute("increment", 0);
+            
+            $this->dom->appendChild($root);
+            
+            $this->dom->save($this->fileName);
         }
     }
 
