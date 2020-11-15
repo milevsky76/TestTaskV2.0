c59823dd07a21656c605223b78c8a5b0b44cc6a4
milevsky76 <33371935+milevsky76@users.noreply.github.com>
2020-11-15 03:30:41 +0300

    Adding a check for file existence

--- Database.php
+++ Database.php
@@ -1,6 +1,17 @@
 <?php
 
     class Database {
+        private $fileName = "users.xml";
+        private $isFileExists;
+        
+        public function __construct(){
+            $this->isFileExists = self::IsFileExists();
+        }
+        
+        //Проверка существования XML файла
+        private function IsFileExists(){
+            return file_exists($this->fileName);
+        }
     }
 
 ?>
